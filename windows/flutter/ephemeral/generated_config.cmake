b35e7036b367b9f4ffeb9cfee00ad6feb34086cd
Asyifa Alfath <asyifa.alfath@students.mikroskil.ac.id>
2025-10-08 15:37:33 +0700

    Initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Asyifa\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\mobile back-end\\voting_app\\voting_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Asyifa\\flutter"
+  "PROJECT_DIR=D:\\mobile back-end\\voting_app\\voting_app"
+  "FLUTTER_ROOT=C:\\Users\\Asyifa\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\mobile back-end\\voting_app\\voting_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\mobile back-end\\voting_app\\voting_app"
+  "FLUTTER_TARGET=D:\\mobile back-end\\voting_app\\voting_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\mobile back-end\\voting_app\\voting_app\\.dart_tool\\package_config.json"
+)
